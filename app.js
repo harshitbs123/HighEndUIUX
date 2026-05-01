df5dabe006ce83803943e6ccd98b8050f5bf6b9b
Harshit Prakash <harshitprakash07@gmail.com>
2026-05-02 04:42:05 +0530

    art's section added

--- app.js
+++ app.js
@@ -20,3 +20,24 @@ window.addEventListener("DOMContentLoaded", () => {
 
   requestAnimationFrame(raf);
 });
+
+// 
+const nav1 = document.getElementById("nav1");
+const nav2 = document.getElementById("nav2");
+const section = document.getElementById("taglineSection");
+
+window.addEventListener("scroll", () => {
+  const position = section.getBoundingClientRect();
+
+  if (position.top <= 0) {
+    // show new navbar
+    nav1.style.opacity = "0";
+    nav2.style.opacity = "1";
+    nav2.style.pointerEvents = "auto";
+  } else {
+    // show old navbar
+    nav1.style.opacity = "1";
+    nav2.style.opacity = "0";
+    nav2.style.pointerEvents = "none";
+  }
+});
